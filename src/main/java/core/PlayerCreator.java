d399d0190b80b9f3722c2a65b1165b71fd039e4c
ashumlyaev <ashumlyaev@geeksforless.net>
2017-05-30 21:01:41 +0300

    Added missed file

---
+++ src/main/java/core/PlayerCreator.java
@@ -0,0 +1,46 @@
+package core;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PlayerCreator {
+    private Properties gameSettings;
+
+    public PlayerCreator(Properties gameSettings) {
+        this.gameSettings = gameSettings;
+    }
+
+    public Player create(String playerName) {
+        ShipBuilder sb = getShipBuilder();
+
+        return new Player(playerName, sb);
+    }
+
+    private ShipBuilder getShipBuilder() {
+        ShipBuilder builder;
+        try {
+            JAXBContext jc = JAXBContext.newInstance(ShipBuilder.class);
+
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            InputStream xml = getClass().getClassLoader().getResourceAsStream("config/ships.settings");
+            builder = (ShipBuilder) unmarshaller.unmarshal(xml);
+
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "ShipBuilder.xsd");
+        } catch (Exception e) {
+            builder = null;
+            e.printStackTrace();
+        }
+
+        if (builder != null) {
+            CoordsBuilder cb = new CoordsBuilder(Integer.parseInt(gameSettings.getProperty("width")), Integer.parseInt(gameSettings.getProperty("height")));
+            builder.setCordsBuilder(cb);
+        }
+
+        return builder;
+    }
+}
